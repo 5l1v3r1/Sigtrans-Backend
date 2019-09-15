b3a80864302d968019bf01dce950a95515d7dbcd
uruburei1 <eduardo_ceep@hotmail.com>
2019-09-15 18:14:02 -0300

    Added Paciente model

---
+++ src/main/java/com/sigtrans/sigtrans/model/saude/paciente/Paciente.java
@@ -0,0 +1,59 @@
+package com.sigtrans.sigtrans.model.saude.paciente;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import java.util.Date;
+
+@Entity
+public class Paciente {
+
+	private @Id
+	@GeneratedValue
+	Long id;
+
+	@Column(name = "nome")
+	private String nome;
+
+	@Column(name = "nome_mae")
+	private String nomeMae;
+
+	@Column(name = "numero_cartao_sus")
+	private String numeroCartaoSus;
+
+	@Column(name = "data_nasc")
+	private Date dataNascimento;
+
+	@ManyToOne
+	private Rua rua;
+
+	@Column(name = "numero")
+	private String numero;
+
+	@Column(name = "complemento")
+	private String complemento;
+
+	@ManyToOne
+	private Bairro bairro;
+
+	@ManyToOne
+	private Municipio municipioOriginal;
+
+	@ManyToOne
+	private Estado estadoOriginal;
+
+	@Column(name = "numero_documento")
+	private String numeroDocumento;
+
+	@Column(name = "idade")
+	private Integer idade;
+
+	@Column(name = "sexo")
+	private char sexo;
+
+	public Paciente() {
+		
+	}
+}
